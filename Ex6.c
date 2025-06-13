6c999fa68658fbd8816b3c4864341b91b2b2b1cd
Eduardo G <eduardogomes5577@gmail.com>
2025-06-13 16:45:40 -0300

    Lista Vetor

---
+++ Ex6.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void troca (int vetor[],int numA, int numB, int *quantTroca,int tamanho);
+void exibir (int vetor[],int tamanho);
+void main()
+{
+    int i,  numA, numB, tamanho, vezes;
+    
+    printf("Entre com os numeros que vão ser trocados de posicao:");
+    scanf("%d %d",&numA, &numB);
+    
+    printf("Entre com o tamaho do vetor:");
+    scanf("%d",&tamanho);
+    int vetor[tamanho];
+    
+    for  (i=0;i<tamanho;i++)
+    {
+        printf("Entre com um valor para preencher o vetor:");
+        scanf("%d",&vetor[i]);
+        
+    }
+    exibir (vetor,tamanho);
+    
+    troca (vetor,numA,numB,&vezes,tamanho);
+    
+    exibir (vetor,tamanho);
+    
+    printf("%d",vezes);
+}
+void exibir (int vetor[],int tamanho)
+{
+    
+    int i;
+    
+    printf("Valores do vetor:");
+    for (i=0;i<tamanho;i++)
+    {
+        printf("%d ",vetor[i]);
+    }
+    printf ("\n\n");
+}
+void troca (int vetor[],int numA, int numB, int *quantTroca,int tamanho)
+{
+    int i,j=100000,k=100000;
+    
+    for (i=0;i<tamanho;i++)
+    {
+        if (vetor[i]==numA)
+        {
+            j=i;
+        }
+        if(vetor[i]==numB)
+        {
+            k=i;
+        }
+    }
+        vetor[j]=numB;
+        vetor[k]=numA;
+        (*quantTroca)=1;
+        //printf("%d",vetor[j]);
+        //printf("%d",vetor[k]);
+}
